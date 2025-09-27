abea627ee2ffd0aa51393e10507403a6c4bcaf11
sakib-ar <sakibrarfile@gmail.com>
2025-09-27 19:10:06 +0600

    2nd Order Regression Function Code

---
+++ Lab Reports/Experiment 05/Exp 05 - Lab Task/poly_reg.m
@@ -0,0 +1,27 @@
+function [a0, a1, a2] = poly_reg(x,y)
+
+n = length(y);
+sum_x = sum(x);
+sum_y = sum(y);
+sum_xsqr = sum(x .^ 2);
+sum_xcube = sum(x .^ 3);
+sum_xpow4 = sum(x .^ 4);
+sum_xy = sum(x .* y);
+sum_xsqry = sum((x .^ 2) .* y);
+
+% Coefficient Matrix
+A = [n sum_x sum_xsqr;
+    sum_x sum_xsqr sum_xcube;
+    sum_xsqr sum_xcube sum_xpow4];
+
+% Constant Matrix
+B = [sum_y;
+    sum_xy;
+    sum_xsqry];
+
+sol = A \ B;
+
+a0 = sol(1);
+a1 = sol(2);
+a2 = sol(3);
+end
