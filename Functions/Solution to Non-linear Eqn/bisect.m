ce384ad56d8126d335b9b1d83e81c72afe322629
sakib-ar <sakibrarfile@gmail.com>
2025-09-27 20:11:24 +0600

    Bisection Function Code

---
+++ Functions/Solution to Non-linear Eqn/bisect.m
@@ -0,0 +1,33 @@
+function [c, itr] = bisect(f,a,b,tol)
+% Inputs:
+% f     --> Function (e.g., @(x) log(3x+2))
+% a,b   --> Lower and Upper Limit
+% tol   --> Tolerance (i.e., stops iteration when reached desired error)
+
+% Outputs:
+% c     --> Approximated Root
+% itr   --> Number of iterations performed
+
+if (f(a) * f(b)) >= 0
+    error('Out of range! Select another Bracket...');
+end
+
+old = 0;
+err = 100;
+itr = 0;
+
+while err > tol
+    itr = itr + 1;
+    c = (a + b) / 2;
+    err = abs((c - old) / c) * 100;
+    old = c;
+
+    if (f(c) * f(a)) > 0
+        a = c;
+    elseif (f(c) * f(a)) < 0
+        b = c;      
+    else
+        break
+    end
+end
+end
